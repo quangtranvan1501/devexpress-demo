05b121159e64e2f8969b83a66566134e64893108
quangtranvan1501 <81863404+quangtranvan1501@users.noreply.github.com>
2024-10-03 14:05:49 +0700

    first commit

---
+++ PredefinedReports/TestReport.cs
@@ -0,0 +1,7 @@
+﻿namespace designerapp.PredefinedReports {
+    public partial class TestReport : DevExpress.XtraReports.UI.XtraReport {
+        public TestReport() {
+            InitializeComponent();
+        }
+    }
+}
